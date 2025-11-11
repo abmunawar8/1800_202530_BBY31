6f19846c24675414099c8130d81cde77a41b4e91
Mischa Potter <mischapotter@icloud.com>
2025-11-11 14:24:03 -0800

    Fixed navbar in filter.html, made selected user skills get saved in a new document under the user collection in set-up-account.html

--- src/components/filter-navbar.js
+++ src/components/filter-navbar.js
@@ -2,7 +2,7 @@ class FilterNavbar extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
     <div
-      class="container-fluid bg-success-subtle p-2 mt-3 d-flex justify-content-between border border-light rounded-5 main-footer"
+      class="container-fluid bg-success-subtle p-2 mt-3 d-flex justify-content-between border border-light rounded-5"
     >
       <button type="button" style="border-radius: 40%;font-size: 15px;font-weight: bold;" onclick="location.href = 'main.html'">Back</button>
       <button type="button" style="border-radius: 40%;font-size: 15px;font-weight: bold;">Filter</button>
@@ -10,5 +10,5 @@ class FilterNavbar extends HTMLElement {
     `;
   }
 }
-
+ 
 customElements.define("filter-navbar", FilterNavbar);
